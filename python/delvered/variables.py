d138a5a9b64e54836a00b7b620b0af71f072ba69
David Nidever <dnidever@noao.edu>
2026-04-02 18:35:12 -0700

    modified variables.getvars() to work with the new exposure-level files

--- python/delvered/variables.py
+++ python/delvered/variables.py
@@ -1,13 +1,18 @@
 import os
 import numpy as np
 from astropy.table import Table,vstack
-from dlnpyutils import utils as dln
+from dlnpyutils import utils as dln,db as dlndb
 from glob import glob
+from delvered import delvered_db as db
 import traceback
 
-def getvars(brick,ndetthresh=20,save=False,overwrite=False):
+def getvars(brick,ndetthresh=20,save=False,overwrite=False,loop=True):
     """ Get variable star data for a brick. """
     bdir = '/net/dl2/dnidever/delve/bricks/'+brick[:4]+'/'+brick
+    metafile = bdir+'/'+brick+'_joint_meta.fits'
+    if os.path.exists(metafile)==False:
+        print(metafile,'NOT FOUND')
+        return
     jobjfile = bdir+'/'+brick+'_joint_object.fits.gz'
     if os.path.exists(jobjfile)==False:
         print(jobjfile,'NOT FOUND')
@@ -20,15 +25,19 @@ def getvars(brick,ndetthresh=20,save=False,overwrite=False):
         return
     print('  ',len(gd),'variables')
     varid = jobj['objid'][gd].data.astype(str)
+    varid = np.array([str(f).strip() for f in varid])  # strip extra spaces
     varobj = jobj[gd]
     
     # Load measurements
     measfile = bdir+'/'+brick+'_joint_meas.fits.gz'
-    if os.path.exists(measfile):
+    if os.path.exists(measfile) and loop==False:
+        print('Using single meas file',measfile)
         meas = Table.read(measfile)
+        for c in meas.colnames: meas[c].name = c.lower()
         meas['objid'] = meas['objid'].data.astype(str)
         index = dln.create_index(meas['objid'])
-        _,ind1,ind2 = np.intersect1d(index['value'],varid,return_indices=True)
+        objid = [str(f).strip() for f in index['value'].astype(str)]
+        _,ind1,ind2 = np.intersect1d(objid,varid,return_indices=True)
         allind = []
         for i in range(len(ind1)):
             ind = index['index'][index['lo'][ind1[i]]:index['hi'][ind1[i]]+1]
@@ -40,18 +49,23 @@ def getvars(brick,ndetthresh=20,save=False,overwrite=False):
     # Split in multiple meas files
     else:
         #measfiles = glob(bdir+'/'+brick+'_joint_meas_????????.fits')
-        measfiles = glob(bdir+'/'+brick+'_joint_meas_*.fits')
-        measfiles.sort()
+        #measfiles = glob(bdir+'/'+brick+'_joint_meas_*.fits')
+        #measfiles.sort()
+        meta = Table.read(metafile,1)
+        for c in meta.colnames:meta[c].name=c.lower()
+        measfiles = Table.read(metafile,2)
+        for c in measfiles.colnames:measfiles[c].name=c.lower()
         meas = []
         for i in range(len(measfiles)):
-            meas1 = Table.read(measfiles[i])
-            expnum1 = os.path.basename(measfiles[i])[:-5].split('_')[-1]
+            filename = str(measfiles['filename'][i]).strip()
+            meas1 = Table.read(filename)
+            for c in meas1.colnames:meas1[c].name=c.lower()
+            expnum1 = os.path.basename(filename)[:-5].split('_')[-1]
             meas1['expnum'] = 8*' '
             meas1['expnum'][:] = expnum1
-            for c in meas1.colnames:meas1[c].name=c.lower()
-            meas1['objid'] = meas1['objid'].data.astype(str)
-            _,ind1,ind2 = np.intersect1d(meas1['objid'],varid,return_indices=True)
-            print('  {:5d}  {:<40s}  {:10s}  {:8d}  {:5d}'.format(i+1,os.path.basename(measfiles[i]),expnum1,len(meas1),len(ind1)))
+            objid = [str(f).strip() for f in meas1['objid'].data.astype(str)]
+            _,ind1,ind2 = np.intersect1d(objid,varid,return_indices=True)
+            print('  {:5d}  {:<40s}  {:10s}  {:8d}  {:5d}'.format(i+1,os.path.basename(filename),expnum1,len(meas1),len(ind1)))
             if len(ind1)>0:
                 meas.append(meas1[ind1])
         meas = vstack(meas)
@@ -71,11 +85,49 @@ def allbricks():
     bricks = Table.read('/home/dnidever/projects/delvered/data/delvemc_bricks_0.25deg.fits.gz')
     for c in bricks.colnames: bricks[c].name = c.lower()
     #for i in range(len(bricks)):
-    for i in range(1528,len(bricks)):
+    for i in range(4332,len(bricks)):
         print(i+1,bricks['brickname'][i])
         try:
             out = getvars(bricks['brickname'][i],save=True,overwrite=True)
-        except KeyboardInterupt:
+        except KeyboardInterrupt:
             raise
         except:
             traceback.print_exc()
+
+def load_database(dbfile=None):
+    """" Load all of the brick variable star data into an sqlite3 database """
+
+    if dbfile is None:
+        dbfile = '/net/dl2/dnidever/delve/bricks/variables/delvemc_variables.db'
+    if os.path.exists(dbfile):
+        print(dbfile,'already exists')
+        return
+
+    bricks = Table.read('/home/dnidever/projects/delvered/data/delvemc_bricks_0.25deg.fits.gz')
+    for c in bricks.colnames: bricks[c].name = c.lower()
+
+    for i in range(len(bricks)):
+        name = bricks['brickname'][i]
+        bdir = '/net/dl2/dnidever/delve/bricks/'
+        bdir = os.path.join(bdir,name[:4],name)
+        measfile = bdir+'/'+name+'_variables_meas.fits'
+        objfile = bdir+'/'+name+'_variables_object.fits'
+        if os.path.exists(measfile) and os.path.exists(objfile):
+            meas = Table.read(measfile)
+            obj = Table.read(objfile)
+            obj['brick'] = name
+            print(i+1,name,len(obj),len(meas))
+            db.writecat2db(meas,dbfile,'meas')
+            db.writecat2db(obj,dbfile,'object')
+        else:
+            print(i+1,name,'variable files not found')
+
+    # make indices
+    print('Indexing')
+    db.createindexdb(dbfile,'ra','object',unique=False)
+    db.createindexdb(dbfile,'dec','object',unique=False)
+    db.createindexdb(dbfile,'objid','object')
+    dlndb.analyzetable(dbfile,'object')
+    db.createindexdb(dbfile,'objid','meas',unique=False)
+    dlndb.analyzetable(dbfile,'meas')
+
