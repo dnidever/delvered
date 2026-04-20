322ec6ba90d3cbe751715506a3809a0d3818e0f2
David Nidever <dnidever@noao.edu>
2026-04-20 11:55:58 -0700

    vardb.py updates

--- python/delvered/vardb.py
+++ python/delvered/vardb.py
@@ -19,6 +19,14 @@ from astropy.coordinates import SkyCoord
 import sqlite3
 import traceback
 
+hostname = socket.gethostname()
+host = hostname.split('.')[0]
+if host=='thing' or host=='hulk' or host=='gp09':
+    DBFILE = '/net/dl2/dnidever/delve/bricks/variables/delvemc_variables.db'
+elif host=='tempest':
+    DBFILE = '/home/group/davidnidever/nsc_variables/delvemc/delvemc_variables.db'
+else:
+    print('no dbfile')
 
 def query(sql):
     """ Get rows from the database """
@@ -30,7 +38,7 @@ def query(sql):
     sqlite3.register_adapter(np.float16, float)
     sqlite3.register_adapter(np.float32, float)
     sqlite3.register_adapter(np.float64, float)
-    db = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    db = sqlite3.connect(DBFILE, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     cur = db.cursor()
 
     # Convert numpy data types to sqlite3 data types
@@ -39,13 +47,72 @@ def query(sql):
     # Execute the select command
     cur.execute(sql)
     data = cur.fetchall()
+    cols = [desc[0] for desc in cur.description]
 
     # No results
     if len(data)==0:
         return np.array([])
 
     # Get table column names and data types
-    cur.execute("select sql from sqlite_master where tbl_name = '"+table+"'")
+    #cur.execute("select sql from sqlite_master where tbl_name = '"+table+"'")
+    #dum = cur.fetchall()
+    #db.close()
+    #head = dum[0][0]
+    ## 'CREATE TABLE exposure(expnum TEXT, nchips INTEGER, filter TEXT, exptime REAL, utdate TEXT, uttime TEXT, airmass REAL, wcstype TEXT)'
+    #lo = head.find('(')
+    #hi = head.find(')')
+    #head = head[lo+1:hi]
+    #cols = head.split(',')
+
+    # infer types from first non-None value
+    types = []
+    for col_idx in range(len(cols)):
+        col_values = [row[col_idx] for row in data if row[col_idx] is not None]
+        inferred = type(col_values[0]) if col_values else None
+        types.append(inferred)
+
+    dt = []
+    for i in range(len(cols)):
+        dt.append( (cols[i], types[i]) )
+    dtype = np.dtype(dt)
+
+    # Convert to nump structured array
+    tab = np.zeros(len(data),dtype=dtype)
+    tab[...] = data
+    del(data)
+
+    return tab    
+
+
+def getobject(objid,verbose=False):
+    """ Get rows from the database """
+    t0 = time.time()
+    sqlite3.register_adapter(np.int8, int)
+    sqlite3.register_adapter(np.int16, int)
+    sqlite3.register_adapter(np.int32, int)
+    sqlite3.register_adapter(np.int64, int)
+    sqlite3.register_adapter(np.float16, float)
+    sqlite3.register_adapter(np.float32, float)
+    sqlite3.register_adapter(np.float64, float)
+    db = sqlite3.connect(DBFILE, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    cur = db.cursor()
+
+    # Convert numpy data types to sqlite3 data types
+    d2d = {"TEXT":(str,100), "INTEGER":int, "REAL":float}
+
+    sql = "SELECT * from object WHERE objid='"+str(objid)+"'"
+
+    # Execute the select command
+    #print(sql)
+    cur.execute(sql)
+    data = cur.fetchall()
+
+    # No results
+    if len(data)==0:
+        return np.array([])
+
+    # Get table column names and data types
+    cur.execute("select sql from sqlite_master where tbl_name = 'object'")
     dum = cur.fetchall()
     db.close()
     head = dum[0][0]
@@ -68,9 +135,10 @@ def query(sql):
 
     if verbose: print('got data in '+str(time.time()-t0)+' sec.')
 
-    return tab    
+    return tab
 
-def getlc(dbfile,table='meas',cols='*',rar=None,decr=None,where=None,verbose=False):
+
+def getlc(objid,verbose=False):
     """ Get rows from the database """
     t0 = time.time()
     sqlite3.register_adapter(np.int8, int)
@@ -80,38 +148,17 @@ def getlc(dbfile,table='meas',cols='*',rar=None,decr=None,where=None,verbose=Fal
     sqlite3.register_adapter(np.float16, float)
     sqlite3.register_adapter(np.float32, float)
     sqlite3.register_adapter(np.float64, float)
-    db = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    db = sqlite3.connect(DBFILE, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     cur = db.cursor()
 
     # Convert numpy data types to sqlite3 data types
     d2d = {"TEXT":(str,100), "INTEGER":int, "REAL":float}
 
-    # Start the SELECT statement
-    cmd = 'SELECT '+cols+' FROM '+table
-    # RA constraints
-    if rar is not None:
-        if cmd.find('WHERE') == -1:
-            cmd += ' WHERE '
-        else:
-            cmd += ' AND '
-        cmd += 'ra>='+str(rar[0])+' AND ra<'+str(rar[1])
-    # DEC constraints
-    if decr is not None:
-        if cmd.find('WHERE') == -1:
-            cmd += ' WHERE '
-        else:
-            cmd += ' AND '
-        cmd += 'dec>='+str(decr[0])+' AND dec<'+str(decr[1])
-    # WHERE
-    if where is not None:
-        if cmd.lower().find('where')>-1:
-            cmd += ' AND '+where
-        else:
-            cmd += ' WHERE '+where
+    sql = "SELECT * from meas WHERE objid='"+str(objid)+"'"
 
     # Execute the select command
-    #print('CMD = '+cmd)
-    cur.execute(cmd)
+    #print(sql)
+    cur.execute(sql)
     data = cur.fetchall()
 
     # No results
@@ -119,7 +166,7 @@ def getlc(dbfile,table='meas',cols='*',rar=None,decr=None,where=None,verbose=Fal
         return np.array([])
 
     # Get table column names and data types
-    cur.execute("select sql from sqlite_master where tbl_name = '"+table+"'")
+    cur.execute("select sql from sqlite_master where tbl_name = 'meas'")
     dum = cur.fetchall()
     db.close()
     head = dum[0][0]
